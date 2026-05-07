da9ea640f3074c4d9f983ef04c34ab974dbec16f
Joe Huss <detain@interserver.net>
2026-05-07 05:56:41 -0400

    Add Korean locale for all libs (#257)

---
+++ lang/ko.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Korean translations for candy-mines.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'board.too_small'           => '보드가 너무 작습니다',
+    'board.minecount_out_of_range' => 'mineCount가 범위를 벗어남',
+];
